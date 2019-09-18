92445c0886043a578c94d83a9f117d0e99f1c9b4
judyluvbot <di.zhou22@ycdsbk12.ca>
2019-09-18 08:58:21 -0400

    print examples

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,11 @@
+print("This is a print statement")
+print("here are the groups i stah bts, twice, itzy, mamamoo.")
+print("here is the name of the song i really like; all about you by taeyeon.")
+greeting = "stream fancy by twice!"
+print(greeting)
+my_number = 50
+print(my_number)
+
+colour = "black"
+print("my shoes are", colour)
+print("stan IU for clear skin")
