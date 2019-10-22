714422ee1ac3d09a34472bba1e4cda33056d09a3
judyluvbot <di.zhou22@ycdsbk12.ca>
2019-10-22 09:30:30 -0400

    update

---
+++ Working/ClassExamples/2_3_6_Comparison_Operators.py
@@ -0,0 +1,8 @@
+table_name = "Shonda"
+
+name = input("Enter your name: ")
+
+if table_name == name:
+    print("Right this way!")
+else: 
+    print("Sorry, we don't have a reservation under that name.")
