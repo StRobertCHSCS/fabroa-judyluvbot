dc56758067e04c98490d8f8919c13392c5607f1b
judyluvbot <di.zhou22@ycdsbk12.ca>
2019-10-29 09:54:57 -0400

    happy

--- Working/Assignments/microbit /microbit_logic_assignment.py
+++ Working/Assignments/microbit /microbit_logic_assignment.py
@@ -1,17 +1,53 @@
 from microbit import *
+import music 
 
-# Microbit exchange words 
-signal = none
+#assign pins 
+sound = pin0
+moisture_sensor = pin2
+led_red = pin3 
+led_green = pin4 
+servo = pin8
+
+tune = ["C4:4", "D", "E", "C", "C", "D", "E", "C", "E", "F", "G:8",
+        "E:4", "F", "G:8"]
+tune2 = ["D4:4", "E4:4", "F4:4", "D4:4", "E4:4", "F4:4", "D4:4", "E4:4",
+        "F4:4", "G4:4", "D4:8", "G4:4", "D4:4", "G4:8"]
+
 while True:
-    if button_a.is_pressed():
-        pin1.write_digital(1)
-        sleep(100)
-        pin1.write_digital(0)
- 
-        signal = pin2.read_digital()
-        if signal == 1:
-            display.scroll("Hello")
-        else: 
-            display.scroll("Error")
+    #sensor temperature 
+    if button_a.is_pressed:
+        #check if temperature is equal to 30 degrees celecius
+        if microbit.temperature.read_digit ==  30:
+            led_green(4)
+            music.play(tune)
+            display.scroll("The temperature is perfect!")
+        elif microbit.temperature.read_digit > 30:
+            display.scroll("Your tempeture is", microbit.temperature.read_digit)
+            display.scroll("Put your plant somewhere cooler!!") 
+            led_red(3)
+            music.play(tune2)
+        else:
+            display.scroll("Your tempeture is", microbit.temperature.read_digit)
+            display.scroll("Put your plant somewhere hotter!!") 
+            led_red(3)
+            music.play(tune2)
 
-display.clear()
+       
+    elif button_b.is_pressed: 
+        #check if the moisture level is equal to 50.
+        if mositure_sensor.read_digit() == 50: 
+            led_green(4)
+            music.play(tune)
+            display.scroll("The moisture is perfect!")
+        elif mositure_sensor.read_digit() > 50:
+            display.scroll("Your moisture level is", moisture_sensor.read_digit)
+            display.scroll("Water your plant less!!") 
+            led_red(3)
+            music.play(tune2)
+        else: 
+            display.scroll("Your moisture level is", moisture_sensor.read_digit)
+            display.scroll("Water your plant more!!") 
+            led_red(3)
+            music.play(tune2)
+    else: 
+        display.scroll("Error!!")
