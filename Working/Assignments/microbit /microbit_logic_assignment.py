bf862b07a26879f3fb89017ac52574d64943dae0
judyluvbot <di.zhou22@ycdsbk12.ca>
2019-10-30 09:42:55 -0400

    pls kms i want to cry

--- Working/Assignments/microbit /microbit_logic_assignment.py
+++ Working/Assignments/microbit /microbit_logic_assignment.py
@@ -3,9 +3,9 @@ import music
 
 #assign pins 
 sound = pin0
-moisture_sensor = pin2
-led_red = pin3 
-led_green = pin4 
+moisture_sensor = pin1
+led_red = pin2
+led_green = pin3 
 servo = pin8
 
 tune = ["C4:4", "D", "E", "C", "C", "D", "E", "C", "E", "F", "G:8",
@@ -17,37 +17,37 @@ while True:
     #sensor temperature 
     if button_a.is_pressed:
         #check if temperature is equal to 30 degrees celecius
-        if microbit.temperature.read_digit ==  30:
-            led_green(4)
-            music.play(tune)
-            display.scroll("The temperature is perfect!")
-        elif microbit.temperature.read_digit > 30:
-            display.scroll("Your tempeture is", microbit.temperature.read_digit)
-            display.scroll("Put your plant somewhere cooler!!") 
-            led_red(3)
-            music.play(tune2)
-        else:
-            display.scroll("Your tempeture is", microbit.temperature.read_digit)
-            display.scroll("Put your plant somewhere hotter!!") 
-            led_red(3)
-            music.play(tune2)
+         if accelerometer.was_gesture('shake'):
+            display.scroll(temperature())
+            if temperature.read_digital() ==  30:
+                led_green.write_digital(3)
+                music.play(music.tune)
+                display.scroll("The temperature is perfect!")
+            elif temperature.read_digital() > 30:
+                display.scroll("Put your plant somewhere cooler!!") 
+                led_red.write_digital(2)
+                music.play(tune2)
+            else:
+                display.scroll("Put your plant somewhere hotter!!") 
+                led_red(2)
+                music.play(tune2)
 
        
     elif button_b.is_pressed: 
         #check if the moisture level is equal to 50.
-        if mositure_sensor.read_digit() == 50: 
-            led_green(4)
+        if mositure_sensor.read_digital() == (50): 
+            led_green(3)
             music.play(tune)
             display.scroll("The moisture is perfect!")
-        elif mositure_sensor.read_digit() > 50:
+        elif mositure_sensor.read_digital() > 50:
             display.scroll("Your moisture level is", moisture_sensor.read_digit)
             display.scroll("Water your plant less!!") 
-            led_red(3)
+            led_red(2)
             music.play(tune2)
         else: 
             display.scroll("Your moisture level is", moisture_sensor.read_digit)
             display.scroll("Water your plant more!!") 
-            led_red(3)
+            led_red(2)
             music.play(tune2)
     else: 
         display.scroll("Error!!")
