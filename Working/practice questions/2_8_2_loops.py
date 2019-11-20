3c896f7405e94e658c39c2a6c00b65ec58a50832
judyluvbot <di.zhou22@ycdsbk12.ca>
2019-11-20 09:47:41 -0500

    ahaha what if we cried im just kidding unless ...

---
+++ Working/practice questions/2_8_2_loops.py
@@ -0,0 +1,14 @@
+# get n
+number = int(input("Enter a number: "))
+
+# initalize total
+total = 0
+
+# compute the total from 1 to n
+for i in range(1, number+1, 2):
+    print(i)
+    total = total + i
+
+
+# output total
+print(total)
