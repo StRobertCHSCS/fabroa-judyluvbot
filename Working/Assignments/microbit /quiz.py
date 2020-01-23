5582292ddd0a3ad7053d14f99d3959fd8bbf9942
judyluvbot <di.zhou22@ycdsbk12.ca>
2020-01-23 09:47:35 -0500

    assignment

--- Working/Assignments/microbit /quiz.py
+++ Working/Assignments/microbit /quiz.py
@@ -4,13 +4,160 @@ print(" ***** Welcome to the Friendly Vibes ******")
 # intalize counters
 total = 0
 
-# set okay answers
-okayanswers = str("yes") or str("no")
-
 # ask question
-answer = str(input("Are you confident about yourself?"))
-for i in range(okayanswers):
-    if answer = yes:
+question1 = "Are you confident about yourself?"
+print(question1)
+
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
         total += 10
-    elif answer = no:
+        break
+    elif reponse == "no":
         total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+question2 = ("Do you think you like change?")
+print(question2)
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
+        total += 10
+        break
+    elif reponse == "no":
+        total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+question3 = ("Is your favourite season summer?")
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
+        total += 10
+        break
+    elif reponse == "no":
+        total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+print(question3)
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
+        total += 10
+        break
+    elif reponse == "no":
+        total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+question4 = ("Is your favourite season Fall?")
+print(question4)
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
+        total += 10
+        break
+    elif reponse == "no":
+        total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+question5 = ("Do you enjoy talking to men more female?")
+print(question5)
+while True:
+    response = input("Enter yes or no. ")
+
+    if response == "yes":
+        total += 10
+        break
+    elif reponse == "no":
+        total += 20
+        break
+    else:
+        print("Incorrect!!! Enter yes or no!")
+
+        while True:
+            response = input("Enter yes or no. ")
+
+            if response == "yes":
+                total += 10
+                stop = True
+                break
+            elif reponse == "no":
+                total += 20
+                break
+        if stop:
+            break
+print("Your score is " + str(total) + " and you are matched with Kevin.")
